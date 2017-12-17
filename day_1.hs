8ece6c2334ef0f7873b234a6e0366668361b5684
Saysil <saesilin@gmail.com>
2017-12-17 18:58:48 -0500

    Finished day 1.

--- day_1.hs
+++ day_1.hs
@@ -12,5 +12,18 @@ getSum length (a:(b:bs))
 sumString :: String -> Int
 sumString str = getSum (length str) (cycle (map digitToInt str))
 
-main = (putStrLn.show) (sumString "112232221111")
+getSum2 :: Int -> Int -> [Int] -> Int
+getSum2 0 _ _ = 0
+getSum2 remaining skip (x:xs)
+        | x == xs!!(skip-1) = x + getSum2 (remaining-1) skip xs
+        | otherwise = getSum2 (remaining-1) (skip) xs
+
+solve1 :: String -> Int
+solve1 = (sumString.(filter (isAlphaNum)))
+
+solve2 :: String -> Int
+solve2 str = (getSum2 (length str) ((length str) `div` 2) (cycle $ map digitToInt (filter (isAlphaNum) str)))
+
+main = ((readFile "day_1_input.txt") >>= (putStrLn.show.solve2))
+--main = ((readFile "day_1_input.txt") >>= (putStrLn.show.solve1))
 
