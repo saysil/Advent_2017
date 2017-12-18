dae711ff562f059a5c36c1f2ee95db66e00c07c5
Saysil <saesilin@gmail.com>
2017-12-17 23:37:50 -0500

    Finished Day 12

--- day_12.hs
+++ day_12.hs
@@ -1,4 +1,5 @@
 
+import Data.Char
 import Data.HashMap
 
 type PipeID = Int
@@ -18,14 +19,24 @@ countZeroes list x seen
 
 parseTree :: [[String]] -> PipeList
 parseTree [] = empty
-parseTree ((f:_:ls):xs) = union (singleton (read f) (Prelude.map read ls)) (parseTree xs)
+parseTree ((f:_:ls):xs) = union (singleton (read f) (Prelude.map (read.(Prelude.filter isAlphaNum)) ls)) (parseTree xs)
 
 parseWhole :: String -> PipeList
 parseWhole str = parseTree (Prelude.map words (lines str))
 
+countGroups :: PipeID -> PipeList -> Int -> Int
+--countGroups _ empty score = score
+countGroups id list score
+        | Data.HashMap.null list = score
+        | otherwise = countGroups (head (keys newList)) newList (1+score)
+        where newList = difference list (snd (countZeroes list id empty))
+
 solve1 :: String -> Int
 solve1 str = fst $ countZeroes (parseWhole str) 0 empty
 
-main = ((readFile "day_12_input") >>= (putStrLn.show.solve1))
---main = (putStrLn.show) (solve1 "0 <-> 2\n1 <-> 1\n2 <-> 0 3 4\n3 <-> 2 4\n4 <-> 2 3 6\n5 <-> 6\n6 <-> 4 5")
+solve2 :: String -> Int
+solve2 str = countGroups 0 (parseWhole str) 0
+
+main = ((readFile "day_12_input.txt") >>= (putStrLn.show.solve2))
+--main = (putStrLn.show) (solve2 "0 <-> 2\n1 <-> 1\n2 <-> 0 3 4\n3 <-> 2 4\n4 <-> 2 3 6\n5 <-> 6\n6 <-> 4 5")
 
