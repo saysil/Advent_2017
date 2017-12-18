44b744eb086e99d3425f01a0e1a12d09508b6cfe
Saysil <saesilin@gmail.com>
2017-12-18 10:02:00 -0500

    Finished Day 13

--- day_13.hs
+++ day_13.hs
@@ -4,18 +4,27 @@ import Data.Char
 position :: Int -> Int -> Int
 position length time = -(abs ((time `mod` (2*(length-1))) - (length-1))) + (length-1)
 
-caught :: (Int, Int)-> Bool
-caught (depth, length) = (position length depth) == 0
+caught :: (Int, Int) -> Int -> Bool
+caught (depth, length) time = (position length time) == 0
 
 value :: (Int, Int) -> Int
 value (depth, length) = depth*length
 
-run :: [(Int, Int)] -> Int
-run [] = 0
-run (x:xs)
-        | caught x = (value x) + run xs
-        | otherwise = run xs
+run :: Int -> [(Int, Int)] -> Int
+run time (x:[])
+        | caught x time = value x
+        | otherwise = 0
+run time (x@(depth, length):xs)
+        | caught x time = (value x) + run (time + (fst (head xs) - depth)) xs
+        | otherwise = run (time + (fst (head xs) - depth)) xs
 
+shortCircuitRun :: Int -> [(Int, Int)] -> Bool
+shortCircuitRun time (x:[])
+        | caught x time = False
+        | otherwise = True
+shortCircuitRun time (x@(depth, length):xs)
+        | caught x time = False
+        | otherwise = shortCircuitRun (time + (fst (head xs) - depth)) xs
 
 parseLn :: [String] -> (Int, Int)
 parseLn (depth:length:[]) = (read depth, read length)
@@ -25,8 +34,17 @@ parse :: String -> [(Int, Int)]
 parse str = (map (parseLn.words.(filter (not.isPunctuation))) (lines str))
 
 solve1 :: String -> Int
-solve1 = run.parse
+solve1 = (run 0).parse
 
-main = ((readFile "day_13_input.txt") >>= (putStrLn.show.solve1))
---main = (putStrLn.show) (solve1 "0: 3\n1: 2\n4: 4\n6: 4")
+
+checkSolve2 :: [(Int, Int)] -> Int -> Int
+checkSolve2 xs a 
+        | shortCircuitRun a xs = a
+        | otherwise = checkSolve2 xs (a+1)
+
+solve2 :: String -> Int
+solve2 str = checkSolve2 (parse str) 1
+
+main = ((readFile "day_13_input.txt") >>= (putStrLn.show.solve2))
+--main = (putStrLn.show) (solve2 "0: 3\n1: 2\n4: 4\n6: 4")
 
