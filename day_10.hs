148a99b68d2079af850ea3ad460c196e5fd5e332
Saysil <saesilin@gmail.com>
2017-12-22 14:21:28 -0500

    Fixed Day 10 bugs

--- day_10.hs
+++ day_10.hs
@@ -2,6 +2,11 @@
 {-# LANGUAGE ViewPatterns #-}
 
 import Data.Sequence as S
+import Data.Foldable
+import Data.Char
+import Data.Bits
+
+import Numeric
 
 type Position = Int
 type Length = Int
@@ -35,8 +40,35 @@ run init ls = getSeq (Prelude.foldl (helperStep) (0, init, 0) ls)
 
 solve1 :: String -> Int
 solve1 str = (index ls 0)*(index ls 1)
-        where ls = (run defaultList (map read (lines str)))
+        where ls = (run defaultList (map read (words str)))
 
 
-main = ((readFile "day_10_input.txt") >>= (putStrLn.show.solve1))
+standardSuffix = [17, 31, 73, 47, 23]
+
+stringToLengths :: String -> [Length]
+stringToLengths str = (map ord str) ++ standardSuffix
+
+xor16 :: [Int] -> Int
+xor16 ls = foldl xor 0 (ls)
+
+finishHash :: [Int] -> [Int]
+finishHash [] = []
+finishHash ls = (xor16 (Prelude.take 16 ls)):(finishHash (Prelude.drop 16 ls))
+
+toHexHelper :: Int -> String
+toHexHelper n = if n < 16 then ('0':(showHex n "")) else (showHex n "")
+
+toHex :: [Int] -> String
+toHex ls = "0x" ++ concat (map toHexHelper ls)
+
+toBytes :: String -> String
+toBytes str = map chr (map read (words $ map (\x->(if x==',' then ' ' else x)) str))
+
+solve2 :: String -> String
+solve2 str = (toHex.finishHash) (toList ls)
+        where ls = (run defaultList (concat (Prelude.replicate 64 (stringToLengths str))))
+
+--main = ((readFile "day_10_input.txt") >>= (putStrLn.show.stringToLengths))
+main = (putStrLn.show) ((solve2) "130,126,1,11,140,2,255,207,18,254,246,164,29,104,0,224")
+--main = ((readFile "day_10_input.txt") >>= (putStrLn.show.solve2))
 
